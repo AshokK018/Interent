e2dfbefb2cd5483d60c764ad754c98c26ecfab7c
ashok <ashoka17388@gmail.com>
2024-03-28 14:59:46 +0530

    hi

---
+++ boolean.js
@@ -0,0 +1,8 @@
+//boolean
+let isMore = 2 >= 4; // false
+let isLess = 5 < 10; // true
+let bool1 = isMore || isLess; // true
+let bool2 = isMore && isLess; // false
+let isEqual1 = 0 == '0';//true type conversion
+let notEqual1 = 0 != '0'; //false
+let notEqual2 = 0 !== '0'; //true
